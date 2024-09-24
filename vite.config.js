3c51a15acd13b0dd21bf7e97b8012d3d2d0c23ae
Lokesh R <lokesh21031999@gmail.com>
2024-09-24 16:09:15 +0530

    host

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: "terser", // Now that Terser is installed, this will work
-    terserOptions: {
-      compress: {
-        drop_console: true, // Removes console logs in production
-      },
-    },
-  },
-  server: {
-    port: 3000, // Optional: Change server port if needed
-    open: true, // Optional: Open browser automatically
-  },
 });
